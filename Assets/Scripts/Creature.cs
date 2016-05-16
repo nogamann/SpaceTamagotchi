d91ed91551fa78a6a547a559c6ecfa1cc106174b
omrikap <omrikap@gmail.com>
2016-05-16 19:45:04 +0300

    init commit

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : MonoBehaviour {
+
+	// fields (meters):
+	float lovePlayrOne;
+	float lovePlayrTwo;
+	float loveGeneral;
+	float hunger;
+	float health;
+	float joy;
+
+	// methods to implement
+	// play
+		// if play meter on max - health damaged
+
+	// eat
+		// 
+	// takeMedicine
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
