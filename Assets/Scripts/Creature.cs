33110374623b6959eb1ecdfd16b637fc65e4aaee
omrikap <omrikap@gmail.com>
2016-05-23 23:00:39 +0300

    creature script edit

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Creature : MonoBehaviour {
 
-	// fields (meters):
+	// meters
 	public float hunger = 0.5f;
 	public float health = 0.5f;
 	public float joy = 0.5f;
@@ -14,11 +14,18 @@ public class Creature : MonoBehaviour {
 	// TODO: should be updated everytime love changes
 //	public float generalLove = (playrOneLove + playrTwoLove) / 2;
 
-	// a dictionary to hold the available moods of the creature
+	// meters update time
+	public float hungerUpdate;
+	public float healthUpdate;
+	public float joyUpdate;
+	public float playerOneLoveUpdate;
+	public float playrTwoLoveUpdate;
+	public float generalLoveUpdate;
+
+	// A dictionary to hold the available moods of the creature
 	Dictionary<string, Dictionary<string, float>> moods = new Dictionary<string, Dictionary<string, float>> ();
 
-
-	// dictionaries for the moods of the creature. Every possible mood is represented by a dictionary.
+	// Dictionaries for the moods of the creature. Every possible mood is represented by a dictionary.
 	Dictionary<string, float> happy = new Dictionary<string, float> ();
 	Dictionary<string, float> bored = new Dictionary<string, float> ();
 	// TODO add more moods
@@ -26,7 +33,7 @@ public class Creature : MonoBehaviour {
 	/// <summary>
 	/// Initializes the moods dictionaries.
 	/// </summary>
-	private void initMoodDictionaries() {
+	private void InitMoodDictionaries() {
 		// init happy
 		happy.Add("interval", 0.01f);
 		happy.Add("decrease", 0.01f);
@@ -53,6 +60,97 @@ public class Creature : MonoBehaviour {
 	}
 
 	public string currentMood;
+
+	/// <summary>
+	/// eat, play or take medicine
+	/// </summary>
+	/// <param name="thing">food, toy or medicine.</param>
+	void DoAction(ThingObject thing){
+		//TODO: play relevant animation
+
+		health += (thing.Health * Mathf.Pow(health, 3));
+		hunger += (thing.Hunger * Mathf.Pow(hunger, 3));
+		joy += (thing.Joy * Mathf.Pow(joy, 3));
+
+		// TODO: change the love of the current player only
+		playerOneLove += (thing.Love * Mathf.Pow(playerOneLove, 3));
+	}
+
+	/// <summary>
+	/// Calculates the mood score.
+	/// </summary>
+	/// <returns>The mood score, according to the rellevant meters.</returns>
+	/// <param name="mood">string. The mood we want to calculate it's score.</param>
+	private float CalculateMoodScore (string mood) {
+		
+		float joyCoefficient = moods [mood] ["joyCoefficient"];
+		float healthCoefficient = moods [mood] ["healthCoefficient"];
+		float hungerCoefficient = moods [mood] ["hungerCoefficient"];
+		float playerOnelLoveCoefficient = moods [mood] ["playerOnelLoveCoefficient"];
+		float playerTwoLoveCoefficient = moods [mood] ["playerTwoLoveCoefficient"];
+		float generalLoveCoefficient = moods [mood] ["generalLoveCoefficient"];
+
+
+		float moodScore = (1 - health) * healthCoefficient + (1 - hunger) * hungerCoefficient + (1 - generalLove) * generalLoveCoefficient +
+			(1 - joy) * joyCoefficient + (1 - playerOneLove) * playerOnelLoveCoefficient + (1 - playrTwoLove) * playerTwoLoveCoefficient;
+
+		return moodScore;
+	}
+		
+	/// <summary>
+	/// Calculate what is the creature's mood based on his meters, and update currentMood.
+	/// </summary>
+	void CalculateAndUpdateMood(){
+		// TODO maybe need a different init?
+		string nextMood = "bored";
+
+		foreach (var mood in moods) {
+			mood.Value ["score"] = CalculateMoodScore (mood.Key);
+			if (mood.Value["score"] > moods[nextMood]["score"]) {
+				nextMood = mood.Key;
+			}
+		}
+
+		currentMood = nextMood;
+	}
+
+	void UpdateMeters() {
+		float time = Time.time;
+		float joyInterval = moods [currentMood] ["joyInterval"];
+
+		if ((time - joyUpdate) >= joyInterval) {
+			joy -= moods [currentMood] ["joyDecrease"];
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		float time = Time.time;
+		InitMoodDictionaries ();
+
+		// initialize the update times of the meters at initialization time
+		hungerUpdate = time;
+		healthUpdate = time;
+		joyUpdate = time;
+		playerOneLoveUpdate = time;
+		playrTwoLoveUpdate = time;
+		generalLoveUpdate = time;
+
+		// the first mood of the creature at birth is Happy
+		currentMood = "happy";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if the creature is dead
+
+		// update mood
+		CalculateAndUpdateMood ();
+	}
+
+
+	// getters and setters
 
 	public string CurrentMood {
 		get {
@@ -116,88 +214,4 @@ public class Creature : MonoBehaviour {
 			LoveGeneral = value;
 		}
 	}
-
-	/// <summary>
-	/// eat, play or take medicine
-	/// </summary>
-	/// <param name="thing">food, toy or medicine.</param>
-	void DoAction(ThingObject thing){
-		//TODO: play relevant animation
-
-		health += (thing.Health * Mathf.Pow(health, 3));
-		hunger += (thing.Hunger * Mathf.Pow(hunger, 3));
-		// TODO: change the love of the current player only
-		playerOneLove += (thing.Love * Mathf.Pow(playerOneLove, 3));
-
-	}
-
-	/// <summary>
-	/// Calculates the mood score.
-	/// </summary>
-	/// <returns>The mood score, according to the rellevant meters.</returns>
-	/// <param name="mood">string. The mood we want to calculate it's score.</param>
-	private float calculateMoodScore (string mood) {
-		
-		float joyCoefficient = moods [mood] ["joyCoefficient"];
-		float healthCoefficient = moods [mood] ["healthCoefficient"];
-		float hungerCoefficient = moods [mood] ["hungerCoefficient"];
-		float playerOnelLoveCoefficient = moods [mood] ["playerOnelLoveCoefficient"];
-		float playerTwoLoveCoefficient = moods [mood] ["playerTwoLoveCoefficient"];
-		float generalLoveCoefficient = moods [mood] ["generalLoveCoefficient"];
-
-
-		float moodScore = (1 - health) * healthCoefficient + (1 - hunger) * hungerCoefficient + (1 - generalLove) * generalLoveCoefficient +
-			(1 - joy) * joyCoefficient + (1 - playerOneLove) * playerOnelLoveCoefficient + (1 - playrTwoLove) * playerTwoLoveCoefficient;
-
-		return moodScore;
-	}
-		
-	/// <summary>
-	/// Calculate what is the creature's mood based on his meters, and update currentMood.
-	/// </summary>
-	void CalculateAndUpdateMood(){
-		// TODO maybe need a different init?
-		string nextMood = "bored";
-
-		foreach (var mood in moods) {
-			mood.Value ["score"] = calculateMoodScore (mood.Key);
-			if (mood.Value["score"] > moods[nextMood]["score"]) {
-				nextMood = mood.Key;
-			}
-		}
-
-		currentMood = nextMood;
-	}
-
-	// Use this for initialization
-	void Start () {
-		initMoodDictionaries ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//check if the creature is dead
-
-		// update mood
-		CalculateAndUpdateMood ();
-	}
 }
-
-
-//		Dictionary<MoodObject, float> moodsDict;
-
-//		moodsDict.Add(bored, (1 - joy) * 0.8f + (1 - loveGeneral) * 0.2f);
-//		moodsDict.Add(sick, (1 - health) * 0.7f + (1 - hunger) * 0.2f + (1 - loveGeneral) * 0.1f);
-//		moodsDict.Add(hungry, (1 - hunger) * 0.5f + (1 - joy) * 0.3f + (1 - loveGeneral) * 0.1f + (1 - health) * 0.1f);
-//		moodsDict.Add(angry, (1 - loveGeneral) * 0.4f + (1 - joy) * 0.3f + (1 - hunger) * 0.3f);
-//		moodsDict.Add(happy, (hunger + joy + loveGeneral + health) / 4.0f);
-//		moodsDict.Add(sad, 1 - moodsDict[happy]);
-
-//		KeyValuePair<MoodObject, float> highestMood = moodsDict [0];
-//		foreach (KeyValuePair<MoodObject, float> mood in moodsDict) {
-//			if (mood.Value > highestMood.Value) {
-//				highestMood = mood;
-//			}
-//		}
-
-//		currentMood = highestMood;
