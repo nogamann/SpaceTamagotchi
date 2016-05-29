a27c550125d4ec75f028d38fac625d770ee87ad3
omrikap <omrikap@gmail.com>
2016-05-29 15:21:32 +0300

    fixed merge in bars.cs

--- Assets/Scripts/GUI/Bar.cs
+++ Assets/Scripts/GUI/Bar.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
@@ -45,50 +44,3 @@ public class Bar : MonoBehaviour
         
     }
 }
-=======
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-
-public class Bar : MonoBehaviour
-{
-    public enum BarType
-    {
-        HealthBar,
-        LoveBar,
-        HungerBar,
-        JoyBar
-    }
-
-    public BarType barType;
-    public Scrollbar scrollBar;
-    public Creature creature;
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch (barType)
-        {
-            case BarType.HealthBar:
-                scrollBar.size = creature.health;
-                break;
-
-            case BarType.HungerBar:
-                scrollBar.size = creature.hunger;
-                break;
-
-            case BarType.JoyBar:
-                scrollBar.size = creature.joy;
-                break;
-
-            case BarType.LoveBar:
-                scrollBar.size = creature.generalLove;
-                break;
-
-                //TODO add cases for playerOneLove and playerTwoLove
-            default:
-                break;
-        }
-    }
-}
->>>>>>> 3e2669e85947e43ed4a4623dd3e4eeb24c9994d7
