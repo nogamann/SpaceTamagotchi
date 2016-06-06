21a03275ce15d160ebe9f498ba3774715a478fc5
noabarel <noa.barel7@gmail.com>
2016-06-06 11:48:41 +0300

    added GameManager script with money

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int money;
+    private float floatMoney;
+    public float moneyEarnedPerSec;
+    public Text moneyText;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        floatMoney += Time.deltaTime * moneyEarnedPerSec;
+        money = Mathf.FloorToInt(floatMoney);
+        moneyText.text = money + "$";
+    }
+
+}
+
